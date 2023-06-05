7b54a78c610d721470288489a91b309a2d75eb0b
Chansu Kim <ckstn0777@naver.com>
2023-06-05 22:33:20 +0900

    feat: Using remote modules on the host side

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "remoteApp/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Host Application</h1>
+      <Button />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- host/vite.config.js
+++ host/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     federation({
       name: "host-app",
       remotes: {
-        remote_app: "http://localhost:5001/assets/remoteEntry.js",
+        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom"],
     }),
