c41d129cc67a1f7e922d65d5204247dbffe13f90
Chansu Kim <ckstn0777@naver.com>
2023-06-05 22:21:17 +0900

    feat: Configuration Remote Module Federation

--- remote/vite.config.js
+++ remote/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "remote-app",
+      filename: "remoteEntry.js",
+      // Modules to expose
+      exposes: {
+        "./Button": "./src/components/Button",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
